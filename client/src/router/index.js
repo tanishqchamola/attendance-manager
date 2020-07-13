8163c58a66923b6be3bc1a4a8a2570d24bc9845f
Tanishq Chamola <chamola.tanishq@gmail.com>
2020-07-13 20:31:53 +0530

    Minor changes

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,23 +1,29 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Dashboard from "@/views/Dashboard";
+import Mark from "@/views/Mark"
 
 Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: "/",
-    redirect: "/Dashboard",
-  },
-  {
-    path: "/Dashboard",
-    name: "Dashboard",
-    component: Dashboard,
-  },
+    {
+        path: "/",
+        redirect: "/Dashboard",
+    },
+    {
+        path: "/Dashboard",
+        name: "Dashboard",
+        component: Dashboard,
+    },
+    {
+        path: "/Mark",
+        name: "Mark",
+        component: Mark,
+    },
 ];
 
 const router = new VueRouter({
-  routes,
+    routes,
 });
 
 export default router;
--- client/src/views/Mark.vue
+++ client/src/views/Mark.vue
@@ -1,13 +1,35 @@
 <template>
-    
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <select class="form-control">
+          <option disabled selected>Select Course</option>
+          <option value="CS-601">CS-601 Computer Networks and Security</option>
+          <option value="CS-602">CS-602 Linear Algebra and Probability</option>
+          <option value="CS-603">CS-603 Modeling and Simulation</option>
+          <option value="CS-604">CS-604 Compiler Design</option>
+          <option value="CS-605">CS-655C Data Mining and Analysis</option>
+        </select>
+      </div>
+      <div class="col">
+        <div class="form-group">
+          <div class="input-group date" id="datetimepicker1">
+            <input type="text" class="form-control" />
+            <span class="input-group-addon">
+              <span class="glyphicon glyphicon-calendar"></span>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    
-}
+  name: "Mark",
+  created() {
+      $("#datetimepicker1").datetimepicker();
+  }
+};
 </script>
-
-<style scoped>
-
-</style>
